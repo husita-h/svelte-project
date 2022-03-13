48291f4fb72c45fddc385baa1e251113f65676d7
tfukushi <tfukushi@tfukushinoMacBook-Air.local>
2022-03-13 17:31:09 +0900

    Hamburgerの導入
    svelte-hamburgers
    https://www.npmjs.com/package/svelte-hamburgers

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,16 @@
 <script>
     import { Router, Link } from "svelte-routing";
+    // 参考: https://www.npmjs.com/package/svelte-hamburgers
+    import Hamburger from 'svelte-hamburgers';
+    let open;
 </script>
+
+<svelte:head>
+	<!-- Import base css -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/svelte-hamburgers@3/dist/css/base.css" />
+	<!-- Import spin css (spin is default type) -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/svelte-hamburgers@3/dist/css/types/spin.css" />
+</svelte:head>
 
 <header class="bg-primary-300">
     <Router>
@@ -10,4 +20,7 @@
             <Link to="create">Create</Link>
         </nav>
     </Router>
+    <Hamburger bind:open --color="white"/>
 </header>
+
+
