917b407408f0c6547b5ed3f92f120656e727fc19
tfukushi <tfukushi@tfukushinoMacBook-Air.local>
2022-03-13 14:37:39 +0900

    リンクの雛形を作成

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,32 @@
 <script>
-	export let name;
+	export const url = "";
+	import { Router, Link, Route } from "svelte-routing";
 	import Header from "./components/Header.svelte";
 	import Footer from "./components/Footer.svelte";
+	import Home from "./components/Home.svelte";
+	import About from "./components/About.svelte";
+	import Create from "./components/Create.svelte";
+	import Diary from "./components/Diary.svelte";
 </script>
 
 <main>
 	<Header/>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Router url="{url}">
+		<nav>
+			<Link to="/">Home</Link>
+			<Link to="about">About</Link>
+			<Link to="create">Create</Link>
+		</nav>
+		<div>
+			<Route path="diary/:id" component="{Diary}"/>
+			<Route path="create" component="{Create}"/>
+			<Route path="about" component="{About}"/>
+			<Route path="/"><Home/></Route>
+		</div>
+	</Router>
 	<Footer/>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
