5dfffeca92a58ad2468a2aba5be80eed6902e2ca
tfukushi <tfukushi@tfukushinoMacBook-Air.local>
2022-03-13 15:01:05 +0900

    smelteの利用設定

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	export const url = "";
 	import { Router, Route } from "svelte-routing";
+	import "smelte/src/tailwind.css" ;
 	import Header from "./components/Header.svelte";
 	import Footer from "./components/Footer.svelte";
 	import Home from "./components/Home.svelte";
