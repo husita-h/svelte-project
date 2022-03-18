bad3d813da045c4bbb7fe8f282d1ceb71fb11ed3
tfukushi <tfukushi@tfukushinoMacBook-Air.local>
2022-03-19 00:40:03 +0900

    フォーム送信機能の追加したが、エラーで動かない(#10)

--- src/components/links/Create.svelte
+++ src/components/links/Create.svelte
@@ -3,13 +3,18 @@
     let disabled = false;
     // 初期値
     let value = 0;
+
+    function sendForm() {
+        console.log("submit!!!")
+    };
 </script>
 
 <h3>Create</h3>
 <div>{value}</div>
 <!-- bund:valueで、valueの値を動的に取得、反映 -->
-<form>
+<form on:submit={sendForm}|preventDefault={submit}>
     <Slider min="0" max="100" bind:value {disabled} />
     <TextField label="Test label" textarea hint="Put your message" min="1" max="100"/>
+    <Button small type="submit">Send</Button>
 </form>
-<Button small>Send</Button>
+<!-- <Button small type="submit">Send</Button> -->
