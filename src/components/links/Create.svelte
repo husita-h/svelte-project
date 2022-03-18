929df63d5a68fc5d41d172ea722a685f5ec1ebe8
tfukushi <tfukushi@tfukushinoMacBook-Air.local>
2022-03-19 00:55:57 +0900

    スライドバーの不要な記述を削除(#10)

--- src/components/links/Create.svelte
+++ src/components/links/Create.svelte
@@ -3,7 +3,6 @@
     import { TextField, Slider, Button } from "smelte";
     import { userId } from "../../store.js";
 
-    let disabled = false;
     let rate = 0;
     let formBody = 0;
     let uid;
@@ -27,7 +26,7 @@
 <div>{rate}</div>
 <!-- bind:valueで、valueの値を動的に取得、反映 -->
 <form on:submit|preventDefault={sendForm}>
-    <Slider min="0" max="100" bind:value={rate} {disabled} />
+    <Slider min="0" max="100" bind:value={rate} />
     <TextField label="Test label" textarea hint="Put your message" min="1" max="100" bind:value={formBody}/>
     <Button small type="submit">Send</Button>
 </form>
