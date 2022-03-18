8dca03ee27d7e62160d0bbea6c2c262ada6f5e58
tfukushi <tfukushi@tfukushinoMacBook-Air.local>
2022-03-18 23:44:11 +0900

    テキストボックスを作成(#10)

--- src/components/links/Create.svelte
+++ src/components/links/Create.svelte
@@ -1,5 +1,15 @@
 <script>
-
+    import { TextField, Slider, Button } from "smelte";
+    let disabled = false;
+    // 初期値
+    let value = 0;
 </script>
 
-<p>Create</p>
+<h3>Create</h3>
+<div>{value}</div>
+<!-- bund:valueで、valueの値を動的に取得、反映 -->
+<form>
+    <Slider min="0" max="100" bind:value {disabled} />
+    <TextField label="Test label" textarea hint="Put your message" min="1" max="100"/>
+</form>
+<Button small>Send</Button>
