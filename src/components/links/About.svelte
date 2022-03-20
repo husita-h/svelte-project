80c0ec5630ea301386135262097cef3602480c5c
tfukushi <tfukushi@tfukushinoMacBook-Air.local>
2022-03-20 13:42:41 +0900

    取得したメッセージを画面に表示(#12)

--- src/components/links/About.svelte
+++ src/components/links/About.svelte
@@ -1,5 +1,44 @@
 <script>
+    import { Button } from "smelte";
+    import { getTextAtFireStore } from "../../helpers/firebase-firestore.js"
+
+    async function displayList () {
+        let res = await getTextAtFireStore();
+        console.log("each回す前のres", res);
+        if (res) {
+            putInTextArr(res);
+            return;
+        }else{
+            alert("ドキュメントの取得に失敗しました。")
+            return;
+        }
+    }
+    
+    function putInTextArr (arr) {
+        let html_list_tag = null;
+        html_list_tag = document.getElementById("textList");
+        arr.forEach((element) => {
+            console.log("eachで回したあとのres", element)
+            let _text = element[1]["text"];
+            let _created_at = new Date(element[1]["created_at"]);
+            html_list_tag.insertAdjacentHTML("afterend", `<li>${_text}<ul><li>作成日時: ${_created_at}</li></ul></li>`);
+        });
+        return;
+    }
+
+    // function deleteButton (){
+
+    // }
+
+    // function deleteButtonCompletely () {
+
+    // }
 
 </script>
 
-<p>About</p>
+<h3>About</h3>
+<ul id="textList">メッセージ</ul>
+<div>同期ボタン</div>
+<Button on:click={displayList}>Fetch</Button>
+<!-- <Button on:click={deleteButton}>Delete</Button>
+<Button on:click={deleteButtonCompletely}>DeleteCompletely</Button> -->
